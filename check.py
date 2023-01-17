d2e5c7063b17f43ddc6eb6b29ff7f2a30aa79034
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2023-01-17 12:31:58 -0500

    update result inspector

--- check.py
+++ check.py
@@ -1,15 +1,22 @@
 #!python3
 # Python Packages
 import sys
+import argparse
+import subprocess
 from os.path import exists, basename
 from tkinter.filedialog import askopenfilename
 # PIP Packages
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 # Custom Packages
 import env
-
+PROGRNAM_NAME = sys.argv[0]
 RESULT_PATH = sys.argv[1] if len(sys.argv) > 1 else askopenfilename(defaultextension=".npy", initialdir=str(env.RUN_PATH))
+# Inspection
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--plot', type=str, default=None)
+args = parser.parse_args(sys.argv[2:])
 
 if not exists(RESULT_PATH):
 	print("File \"{}\" does not exist".format(RESULT_PATH), file=sys.stderr)
@@ -22,11 +29,30 @@ reference = (reference * 255 / np.max(reference)).astype(np.uint8)
 
 h, w, d = result.shape
 
+if args.plot is not None:
+	x, y = [int(_) for _ in args.plot.split(',')]
+	line1 = np.reshape(reference[x, y, :], (-1))
+	line2 = np.reshape(result[x, y, :], (-1))
+	plt.plot(line1, 'r-')
+	plt.plot(line2, 'b-')
+	plt.show()
+	sys.exit(0)
+
+
 WINDOW_NAME = "Inspecting {}".format(ID)
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
 cv.startWindowThread()
 cv.setWindowProperty(WINDOW_NAME, cv.WND_PROP_TOPMOST, 1)
-for i in range(d):
+
+MARGIN = 20
+user_selection = None
+i = 0
+
+
+def view(_i=None):
+	global i
+	if _i is not None:
+		i = _i
 	img1 = reference[:, :, i]
 	img2 = result[:, :, i]
 	# Compute diff image
@@ -36,11 +62,32 @@ for i in range(d):
 	diff = np.stack((B, G, R), axis=2)
 	# Convert imgs to gray RGB
 	img1, img2 = [np.stack((_, _, _), axis=2) for _ in [img1, img2]]
-	blank = np.zeros((h, 20, 3), dtype=np.uint8)
-	print("Viewing band #{} ({})".format(i, np.max(img2)))
+	if user_selection is not None:
+		img1 = cv.drawMarker(img1.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
+		img2 = cv.drawMarker(img2.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
+		diff = cv.drawMarker(diff.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
+	blank = np.zeros((h, MARGIN, 3), dtype=np.uint8)
+	# print("Viewing band #{} ({})".format(i, np.max(img2)))
 	cv.imshow(WINDOW_NAME, np.concatenate((img1, blank, img2, blank, diff), axis=1))
-	key = cv.waitKey(100)
-	if key == 'q' or key == 27:
-		break
+
+
+def plot_bands(e, x, y, flages, param):
+	global user_selection
+	x = x % (w + MARGIN)
+	user_selection = (x, y)
+	view()
+	if e == cv.EVENT_LBUTTONUP:
+		subprocess.run(['python3', str(env.BASE / PROGRNAM_NAME), RESULT_PATH, '-p', "{},{}".format(x, y)])
+		user_selection = None
+
+
+cv.createTrackbar('select band', WINDOW_NAME , 0, d, view)
+view(0)
+
+cv.setMouseCallback(WINDOW_NAME, plot_bands)
+cv.waitKey(0)
+
 cv.destroyAllWindows()
 cv.waitKey(10)
+
+sys.exit(0)
