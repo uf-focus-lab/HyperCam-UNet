725720a483aa6770a4b5a2ba0f752deb043a8182
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2023-02-20 20:45:20 -0500

    check.py: improve interface

--- check.py
+++ check.py
@@ -1,8 +1,6 @@
-#!python3
+#!python
 # Python Packages
 import sys
-import argparse
-import subprocess
 from os.path import exists, basename
 from tkinter.filedialog import askopenfilename
 # PIP Packages
@@ -11,13 +9,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 # Custom Packages
 import env
-PROGRNAM_NAME = sys.argv[0]
-RESULT_PATH = sys.argv[1] if len(sys.argv) > 1 else askopenfilename(defaultextension=".npy", initialdir=str(env.RUN_PATH))
-# Inspection
-parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--plot', type=str, default=None)
-args = parser.parse_args(sys.argv[2:])
-
+RESULT_PATH = askopenfilename(defaultextension=".npy", initialdir=str(env.RUN_PATH))
+if RESULT_PATH is None:
+	sys.exit(0)
 if not exists(RESULT_PATH):
 	print("File \"{}\" does not exist".format(RESULT_PATH), file=sys.stderr)
 	sys.exit(1)
@@ -29,16 +23,6 @@ reference = (reference * 255 / np.max(reference)).astype(np.uint8)
 
 h, w, d = result.shape
 
-if args.plot is not None:
-	x, y = [int(_) for _ in args.plot.split(',')]
-	line1 = np.reshape(reference[x, y, :], (-1))
-	line2 = np.reshape(result[x, y, :], (-1))
-	plt.plot(line1, 'r-')
-	plt.plot(line2, 'b-')
-	plt.show()
-	sys.exit(0)
-
-
 WINDOW_NAME = "Inspecting {}".format(ID)
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
 cv.startWindowThread()
@@ -47,7 +31,6 @@ cv.setWindowProperty(WINDOW_NAME, cv.WND_PROP_TOPMOST, 1)
 MARGIN = 20
 user_selection = None
 i = 0
-
 
 def view(_i=None):
 	global i
@@ -63,23 +46,40 @@ def view(_i=None):
 	# Convert imgs to gray RGB
 	img1, img2 = [np.stack((_, _, _), axis=2) for _ in [img1, img2]]
 	if user_selection is not None:
-		img1 = cv.drawMarker(img1.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
-		img2 = cv.drawMarker(img2.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
-		diff = cv.drawMarker(diff.astype(np.uint8).copy(), user_selection, (0, 255, 0), cv.MARKER_CROSS, 10, 1)
+		MARKER_SIZE = 16
+		MARKER_WEIGHT = 1
+		img1 = cv.drawMarker(img1.astype(np.uint8).copy(), user_selection, (  0,   0, 255), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+		img2 = cv.drawMarker(img2.astype(np.uint8).copy(), user_selection, (255, 128, 128), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+		diff = cv.drawMarker(diff.astype(np.uint8).copy(), user_selection, (  0, 255,   0), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
 	blank = np.zeros((h, MARGIN, 3), dtype=np.uint8)
 	# print("Viewing band #{} ({})".format(i, np.max(img2)))
 	cv.imshow(WINDOW_NAME, np.concatenate((img1, blank, img2, blank, diff), axis=1))
 
+band_plot = None
+band_global_max = np.max([np.max(reference), np.max(result)])
 
-def plot_bands(e, x, y, flages, param):
+def plot_bands(e, x, y, flags, param):
 	global user_selection
 	x = x % (w + MARGIN)
 	user_selection = (x, y)
 	view()
-	if e == cv.EVENT_LBUTTONUP:
-		subprocess.run(['python3', str(env.BASE / PROGRNAM_NAME), RESULT_PATH, '-p', "{},{}".format(x, y)])
-		user_selection = None
-
+	# Plot on time axis
+	global band_plot
+	global band_global_max
+	PLOT_WIN_NAME = 'Time Axis Plot'
+	if (band_plot is None):
+		band_plot = PLOT_WIN_NAME
+		cv.namedWindow(PLOT_WIN_NAME, cv.WINDOW_AUTOSIZE)
+		cv.startWindowThread()
+	fig = plt.figure()
+	line1 = np.reshape(reference[x, y, :], (-1))
+	line2 = np.reshape(result[x, y, :], (-1))
+	plt.plot(line1, 'r-')
+	plt.plot(line2, 'b-')
+	plt.ylim([0, band_global_max])
+	fig.canvas.draw()
+	fig_img = cv.cvtColor(np.asarray(fig.canvas.buffer_rgba()), cv.COLOR_RGBA2BGR)
+	cv.imshow(PLOT_WIN_NAME, fig_img)
 
 cv.createTrackbar('select band', WINDOW_NAME , 0, d, view)
 view(0)
