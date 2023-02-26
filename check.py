63e22f83a5a847b9302ad35d8d744101b6843d07
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2023-02-26 12:24:40 -0500

    check.py update

--- check.py
+++ check.py
@@ -7,8 +7,10 @@ from tkinter.filedialog import askopenfilename
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 # Custom Packages
 import env
+from param import LED_LIST, REF_BANDS
 CAPTURE_DIR = env.ensureDir(env.VAR_PATH / "capture")
 isNpMtx = lambda f: type(f) == str and f.endswith(".npy") and exists(f)
 askForMtx = lambda: askopenfilename(defaultextension=".npy", initialdir=str(env.RUN_PATH))
@@ -25,11 +27,20 @@ CAPTURE_PREFIX = abspath(dirname(RESULT_PATH))	\
 	.replace('/', '_')
 
 ID = basename(RESULT_PATH).replace(".npy", "")
+
 prediction = np.load(RESULT_PATH)
+h, w, d = prediction.shape
+
+raw = np.load(str(env.DATA_PATH / (ID + ".npy")))
+print(raw.dtype)
+raw = np.stack([
+	Image.fromarray(raw[:, :, d]).resize((w, h))
+	for d in range(raw.shape[2])
+], axis=2)
+print(raw.shape)
 reference = np.load(str(env.DATA_PATH / (ID + "_REF.npy")))
 reference = (reference * 255 / np.max(reference)).astype(np.uint8)
 
-h, w, d = prediction.shape
 
 WINDOW_NAME = "Inspecting {}".format(ID)
 cv.namedWindow(WINDOW_NAME, cv.WINDOW_AUTOSIZE)
@@ -39,7 +50,16 @@ cv.setWindowProperty(WINDOW_NAME, cv.WND_PROP_TOPMOST, 1)
 MARGIN = 20
 user_selection = None
 i = 0
+raw_display = np.stack([raw[:,:,_] for _ in [1,2,6]], axis=2).astype(np.uint8)
 
+C = [
+    ( 64,  64, 255),
+    (200, 255,  64),
+    ( 64, 255, 255),
+    (255, 255, 255),
+]
+def TEXT(im, txt, pos=(10, 10), f=cv.FONT_HERSHEY_DUPLEX, scale=0.5, c=(255, 255, 255), b=1):
+	return cv.putText(im.astype(np.uint8), txt, pos, f, scale, c, b, cv.LINE_AA)
 def view(_i=None):
 	global i
 	if _i is not None:
@@ -53,35 +73,49 @@ def view(_i=None):
 	diff = np.stack((B, G, R), axis=2)
 	# Convert imgs to gray RGB
 	img1, img2 = [np.stack((_, _, _), axis=2) for _ in [img1, img2]]
+	# Put text
+	img1 = TEXT(img1.astype(np.uint8).copy(), "Ground Truth", (10, h-10), c=C[0])
+	_raw = TEXT(raw_display.copy(), "Raw data (RGB)", (10, h-10), c=C[1])
+	img2 = TEXT(img2.astype(np.uint8).copy(), "Model Prediction", (10, h-10), c=C[2])
+	diff = TEXT(diff.astype(np.uint8).copy(), "Difference", (10, h-10), c=C[3])
 	if user_selection is not None:
 		MARKER_SIZE = 16
 		MARKER_WEIGHT = 1
-		img1 = cv.drawMarker(img1.astype(np.uint8).copy(), user_selection, (  0,   0, 255), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
-		img2 = cv.drawMarker(img2.astype(np.uint8).copy(), user_selection, (255,  64,  64), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
-		diff = cv.drawMarker(diff.astype(np.uint8).copy(), user_selection, (  0, 255,   0), cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
-	blank = np.zeros((h, MARGIN, 3), dtype=np.uint8)
-	display_img = np.concatenate((img1, blank, img2, blank, diff), axis=1)
+		img1 = cv.drawMarker(img1, user_selection, C[0], cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+		_raw = cv.drawMarker(_raw, user_selection, C[1], cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+		img2 = cv.drawMarker(img2, user_selection, C[2], cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+		diff = cv.drawMarker(diff, user_selection, C[3], cv.MARKER_CROSS, MARKER_SIZE, MARKER_WEIGHT)
+	vBlank = 255 * np.ones((h, MARGIN, 3), dtype=np.uint8)
+	row1 = np.concatenate((vBlank, img1, vBlank, _raw, vBlank), axis=1)
+	row2 = np.concatenate((vBlank, img2, vBlank, diff, vBlank), axis=1)
+	hBlank = 255 * np.ones((MARGIN, row1.shape[1], 3), dtype=np.uint8)
+	display_img = np.concatenate((hBlank, row1, hBlank, row2, hBlank), axis=0)
 	cv.imshow(WINDOW_NAME, display_img)
 	return display_img
 
 band_plot = None
-band_global_max = np.max([np.max(reference), np.max(prediction)])
-
-PLT_TITLE = 'Pixel Spectrum'
+global_max = [np.max(reference), np.max(raw), np.max(prediction)]
+print(global_max)
+PLT_TITLE = 'Spectrum of Selected Pixel'
 fig = plt.figure()
 plt.title(PLT_TITLE)
 ax = fig.gca()
-l_ref, = ax.plot(range(d), np.zeros(d), 'r-')
-l_prd, = ax.plot(range(d), np.zeros(d), 'b-')
+l_raw, = ax.plot([b for _,b,_ in LED_LIST], np.ones(len(LED_LIST)), 'cx')
+l_ref, = ax.plot(REF_BANDS, np.zeros(d), 'r-')
+l_prd, = ax.plot(REF_BANDS, np.zeros(d), 'b-')
+l_raw.set_label("raw input")
 l_ref.set_label("ground truth")
 l_prd.set_label("prediction")
-ax.set_xlim([0, d-1])
-ax.set_ylim([0, band_global_max])
+ax.set_xlabel("Wavelength (nm)")
+ax.set_ylabel("Intensity (relative)")
+ax.set_xlim([390, 1000])
+ax.set_ylim([0, 1])
 ax.legend()
 
 def plot_bands(e, x, y, flags, param):
 	global user_selection
-	x = x % (w + MARGIN)
+	x = x % (w + MARGIN) - MARGIN
+	y = y % (h + MARGIN) - MARGIN
 	if x < 0 or x >= w or y < 0 or y >= h:
 		user_selection = None
 	else:
@@ -97,8 +131,9 @@ def plot_bands(e, x, y, flags, param):
 		cv.namedWindow(PLT_TITLE, cv.WINDOW_AUTOSIZE)
 		cv.startWindowThread()
 	plt.figure(fig)
-	l_ref.set_ydata(np.reshape(reference[x, y, :], (-1)))
-	l_prd.set_ydata(np.reshape(prediction[x, y, :], (-1)))
+	l_ref.set_ydata(np.reshape(reference[y, x, :], (-1)) / global_max[0])
+	l_raw.set_ydata(np.reshape(raw[y, x, :], (-1)) / global_max[1])
+	l_prd.set_ydata(np.reshape(prediction[y, x, :], (-1)) / global_max[2])
 	fig.canvas.draw()
 	figure_img = cv.cvtColor(np.asarray(fig.canvas.buffer_rgba()), cv.COLOR_RGBA2BGR)
 	cv.imshow(PLT_TITLE, figure_img)
@@ -113,7 +148,7 @@ def plot_bands(e, x, y, flags, param):
 		cv.imwrite(str(file_path), figure_img)
 		print("Selection Saved => {}".format(file_path))
 
-cv.createTrackbar('select band', WINDOW_NAME , 0, d, view)
+cv.createTrackbar('select band', WINDOW_NAME , 0, d-1, view)
 view(0)
 
 cv.setMouseCallback(WINDOW_NAME, plot_bands)
---
+++ param.py
@@ -0,0 +1,13 @@
+"""Name | Bandwidth(nm) | Delta Bandwidth(nm)"""
+LED_LIST = [
+	("U-Violet", 395, 10),
+	("Blue"    , 466, 15),
+	("Green"   , 520, 15),
+	("Y-Green" , 573, 20),
+	("Yellow"  , 585, 20),
+	("Orange"  , 600, 20),
+	("Red"     , 660, 17),
+	("Infrared", 940, 40),
+]
+
+REF_BANDS = list(range(402, 998 + 2, 2))
