e8fe0559c6597f31d25ed5edf067084ca041639e
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2023-04-06 03:10:19 -0400

    update

--- augment.py
+++ augment.py
@@ -10,7 +10,7 @@ def randfloat(l:float, r:float) -> float:
     return l + random() * (r - l)
 
 
-def affine(*samples: Tensor, trans_r=[-0.5, 0.5], scale_r=[0.5, 1.5], shear=[0.5, 1]):
+def affine(*samples: Tensor, trans_r=[-0.25, 0.25], scale_r=[0.8, 2.0], shear=[-30, 30]):
     # -180 ~ +180 degrees
     angle = randfloat(-180, 180)
     # Depending on size of the tensor
@@ -39,9 +39,11 @@ if __name__ == "__main__":
     img = np.logical_xor((x % 100) < 50, (y % 100) < 50)
     img = np.stack([img, ~img, np.zeros(img.shape)], axis=2).astype(np.float32)
     cv2.imshow("original", img)
-    img = from_numpy(img)
+    img = from_numpy(img).swapaxes(0, 2)
     while True:
-        a, = affine(img).detach().numpy().astype(np.float32)
-        cv2.imshow("affine", img)
+        a, = affine(img)
+        a = a.detach().swapaxes(0, 2).numpy().astype(np.float32)
+        cv2.imshow("affine", a)
         k = cv2.waitKey(0)
-        if k != ' ' or k != 13: break
+        if k == ord('q'):
+            break
